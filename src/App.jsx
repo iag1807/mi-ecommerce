f9d3773d0ba1ffe3170d5676ceedf13401587c36
Isabela Acevedo <isabelaagomez1807@gmail.com>
2026-03-24 16:07:37 -0500

    Se crea el componente CategoryFilter.jsx que trae todas las categorias guardandolas en un array de objetos donde valor es el nombre de la categoria de la API y la etiqueta es el nombre que vera el usuario, con cada boton se podra seleccionar una categoria, se crean los estilos para este componente que es CategoryFilter.css y en el App.jsx se importa el componente CategoryFilter.jsx y se mostraran al inicio todas las categoriaso si se selecciona una categoria en especifico se mostraran los productos de esa categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react"
 import { ProductCard } from "./components/ProductCard.jsx"
+import { CategoryFilter } from "./components/CategoryFilter.jsx"
 
 export const App = () => {
 
   const [productos, setProductos] = useState([])
   const [cargando, setCargando] = useState(true)
   const [error, setError] = useState(null)
+  const [categoria, setCategoria] = useState("all")
 
   useEffect(() => {
     fetch("https://dummyjson.com/products?limit=100")
@@ -23,11 +25,15 @@ export const App = () => {
   if (cargando) return <p>Cargando...</p>
   if (error) return <p>{error}</p>
 
+  const productosFiltrados = productos
+    .filter(p => categoria === "all" || p.category === categoria)
+
   return (
     <div className="container">
       <h1 className="titulo">Mini Ecommerce</h1>
+      <CategoryFilter setCategoria={setCategoria} />
       <div className="grid">
-        {productos.map(p => (
+        {productosFiltrados.map(p => (
           <ProductCard key={p.id} producto={p} />
         ))}
       </div>
--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,7 +1,27 @@
-export const CategoryFilter = () => {
+import "../styles/CategoryFilter.css"
+
+export const CategoryFilter = ({ setCategoria }) => {
+
+  const categorias = [
+    { valor: "all", etiqueta: "Todas" },
+    { valor: "beauty", etiqueta: "Maquillaje" },
+    { valor: "fragrances", etiqueta: "Perfumes" },
+    { valor: "laptops", etiqueta: "Laptops" },
+    { valor: "mobile-accessories", etiqueta: "Accesorios Moviles" },
+    { valor: "furniture", etiqueta: "Muebles" },
+    { valor: "home-decoration", etiqueta: "Decoracion del hogar" },
+    { valor: "kitchen-accessories", etiqueta: "Accesorios de cocina" },
+    { valor: "groceries", etiqueta: "Comida" },
+  ]
+
   return (
-    <div>
-        <h1>Categorias</h1>
+    <div className="categorias">
+      <h1 className="titulo">Categorias</h1>
+      {categorias.map(c => (
+        <button key={c.valor} onClick={() => setCategoria(c.valor)}>
+          {c.etiqueta}
+        </button>
+      ))}
     </div>
   )
 }
